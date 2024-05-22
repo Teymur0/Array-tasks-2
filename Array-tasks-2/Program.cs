8cc45177f7f56e1b06b72617c09d2fe090ade42c
Teymur0 <tnabiev000@gmail.com>
2024-05-22 16:00:06 +0400

    Add project files.

---
+++ Array-tasks-2/Program.cs
@@ -0,0 +1,72 @@
+﻿#region REVERSE ARRAY
+//int[] arr = { 1, 2, 3, 4, 5, 4 };
+//for (int i = arr.Length - 1; i >= 0; i--)
+//{
+//    int reversedArr = arr[i];
+//    Console.Write(reversedArr + " ");
+//}
+
+#endregion
+
+#region ARRAY ELEMNTS MATCH
+//int[] firstArr = { 5, 3, 1 };
+//int[] secondArr = { 1, 2, 3, 5 };
+//bool isMatch = false;
+
+//for (int i = 0; i < firstArr.Length; i++)
+//{
+//    bool isfound = false;
+
+//    for (int j = 0; j < secondArr.Length; j++)
+//    {
+//        if (firstArr[i] == secondArr[j])
+//        {
+//            isMatch = true;
+//            break;
+//        }
+//        else
+//        {
+//            isMatch = false;
+//            break;
+//        }
+
+
+//    }
+
+//}
+//if (isMatch)
+//{
+//    Console.WriteLine("elemets are mathces");
+//}
+//else
+//{
+//    Console.WriteLine(" no matches");
+//}
+
+#endregion
+
+#region ARRAY ELEMNT DUPLICATION
+
+//int[] arr = { 1, 2, 3, 5,5 6 };
+//int duplicatedEl=0;
+//bool isDuplicated=false;
+
+//for (int i = 0; i < arr.Length-1; i++)
+//{
+
+//    if (arr[i] == arr[i+1])
+//    {
+//        duplicatedEl = arr[i];
+//        isDuplicated = true;
+//    }
+//}
+//if (isDuplicated)
+//{
+//    Console.WriteLine(duplicatedEl);
+//}
+//else
+//{
+//    Console.WriteLine("No Duplications");
+//}
+
+#endregion
